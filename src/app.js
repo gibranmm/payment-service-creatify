dc55b3233c1bd11115d63aaf8bcd5e2d2208c262
Gibran <gibranmaulanam@gmail.com>
2025-07-11 13:27:57 +0700

    second commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,13 @@ const paymentRoutes = require('./routes/paymentRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
